6fa26de4648a5284022c90d20075f58050b38907
Lungelo <iamlouisthabethe@gmail.com>
2024-09-04 19:28:44 +0200

    routine commit

--- src/main/java/com/example/todo/controller/user/DeleteServlet.java
+++ src/main/java/com/example/todo/controller/user/DeleteServlet.java
@@ -33,8 +33,8 @@ public class DeleteServlet extends HttpServlet {
                 response.sendRedirect("settings");
             } else {
                 Errors.fail(response, request);
-                response.getWriter().println("Error deleting user");
-                System.out.println("Error deleting user");
+                response.getWriter().println("Error deleting user.");
+                System.out.println("Error deleting user.");
                 System.out.println(sessionUsername);
                 System.out.println(UserManager.sessionEmail);
             }
